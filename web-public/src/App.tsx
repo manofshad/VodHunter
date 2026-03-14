6b31ae77f8682da33c1fbcedd19fe0c75470fb99
Shadman Farhan <43256895+manofshad@users.noreply.github.com>
2026-03-14 17:54:14 -0400

    Redo public front end

--- web-public/src/App.tsx
+++ web-public/src/App.tsx
@@ -1,17 +1,5 @@
 import SearchPage from "./components/SearchPage";
 
 export default function App() {
-  return (
-    <div className="app-shell">
-      <header className="navbar">
-        <div className="navbar-inner">
-          <div className="brand" aria-label="VodHunter">
-            <span className="brand-primary">Vod</span>
-            <span className="brand-accent">Hunter</span>
-          </div>
-        </div>
-      </header>
-      <SearchPage />
-    </div>
-  );
+  return <SearchPage />;
 }
